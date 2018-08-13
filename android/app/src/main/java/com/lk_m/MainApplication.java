17ec8958be668f7ebe768f07d77bb70bde543db0
spirit <catchspirit37@gmail.com>
2018-08-13 14:08:29 +0800

    z

--- android/app/src/main/java/com/lk_m/MainApplication.java
+++ android/app/src/main/java/com/lk_m/MainApplication.java
@@ -16,6 +16,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import cn.reactnative.modules.update.UpdateContext;
+
 
 import java.util.Arrays;
 import java.util.List;
