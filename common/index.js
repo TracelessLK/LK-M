b06cb4c2d5a57d3176ff91478781ca948fd5773d
spirit <catchspirit37@gmail.com>
2018-08-24 18:39:41 +0800

    z

--- common/index.js
+++ common/index.js
@@ -3,9 +3,8 @@ import GroupAvatar from './widget/GroupAvatar'
 import ActivityIndicator from './widget/ActivityIndicator'
 import List from './widget/List'
 import MessageList from './widget/MessageList'
-import ScanView from './view/ScanView'
 import LoadingView from './view/LoadingView'
-const commonUtil = require('./util/commonUtil')
+const {ScanView, commonUtil} = require('@hfs/react-native-collection')
 const updateUtil = require('./util/updateUtil')
 const commonStyle = require('./view/style')
 
