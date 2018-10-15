4e5cffa9e1a4eb6bae59568a12326aa2685a59ce
spirit <catchspirit37@gmail.com>
2018-10-15 15:51:00 +0800

    z - Mon Oct 15 2018 15:50:59 GMT+0800 (CST)

--- lk/view/auth/PasswordLoginView.js
+++ lk/view/auth/PasswordLoginView.js
@@ -1,48 +1,94 @@
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
-    Alert,
-    AsyncStorage,
-    Button,
-    NativeModules,
-    Platform,
-    ScrollView,
-    StyleSheet,
-    Text,
-    View,Modal,
-} from 'react-native';
 
+  Text,
+  TextInput,
+  View
+} from 'react-native'
+import {
+  Card,
+  CardItem,
+  Button,
+  Toast
+} from 'native-base'
+const md5 = require('crypto-js/md5')
+const Application = require('../../LKApplication')
+const lkApplication = Application.getCurrentApp()
 
 export default class PasswordLoginView extends Component<{}> {
-
-    constructor(props){
-        super(props);
-        this.state={};
+  static navigationOptions =() => {
+    return {
+      headerTitle: '登录'
     }
+  }
+  constructor (props) {
+    super(props)
+    const {user} = this.props.navigation.state.params
+    this.user = user
+    this.state = {}
+    this.t = ''
+  }
 
-    componentDidMount(){
+  render () {
+    const {navigation} = this.props
+    return (
+      <View style={{alignItems: 'center', marginVertical: 40}}>
+        <Card style={{width: '90%'}}>
+          <CardItem header bordered>
+            <Text>请输入登录密码</Text>
+          </CardItem>
+          <CardItem bordered>
+            <View style={{justifyContent: 'center', flexDirection: 'row', alignItems: 'center'}}>
+              <TextInput style={{
+                flex: 1,
+                color: 'black',
+                fontSize: 16,
+                paddingHorizontal: 4,
+                borderWidth: 1,
+                borderColor: '#d0d0d0',
+                borderRadius: 5,
+                marginHorizontal: 5,
+                minHeight: 40,
+                backgroundColor: '#f0f0f0',
+                marginBottom: 5
+              }} autoFocus secureTextEntry onChangeText={t => {
+                this.t = t
+              }}
+              ></TextInput>
+              <Button block info style={{width: 60, height: 40}} onPress={() => {
+                console.log({t:this.t})
+                if (!this.t) {
+                  Toast.show({
+                    text: '请输入密码',
+                    position: 'top'
+                  })
+                } else {
+                  const password = md5(this.t).toString()
+                  if (password === this.user.password) {
+                    lkApplication.setCurrentUser(this.user)
+                    navigation.navigate('MainStack')
+                  } else {
+                    Toast.show({
+                      text: '密码错误,请重试',
+                      position: 'top'
+                    })
+                  }
+                }
+              }}>
+                <Text style={{color: 'white'}}>确认</Text>
+              </Button>
+            </View>
 
-    }
-
-    componentDidUpdate(){
-    }
-
-
-    componentWillUnmount(){
-
-    }
-
-    render() {
-        return (
-           <View>
-           </View>
-        )
-    }
-
+          </CardItem>
+        </Card>
+      </View>
+    )
+  }
 }
 
 PasswordLoginView.defaultProps = {
-    
+
 }
 
 PasswordLoginView.propTypes = {
--- lk/view/auth/SelectUserView.js
+++ lk/view/auth/SelectUserView.js
@@ -15,17 +15,21 @@ import PropTypes from 'prop-types'
 import userProvider from '../../logic/provider/LKUserProvider'
 const Application = require('../../LKApplication')
 const lkApplication = Application.getCurrentApp()
+const common = require('@external/common')
+const {List, MessageList} = common
+const {getAvatarSource} = require('../../util')
 
 export default class SelectUserView extends Component<{}> {
   static navigationOptions =() => {
     return {
-      headerTitle: '请选择账户登录'
+      headerTitle: '选择账户'
     }
   }
   constructor (props) {
     super(props)
-    this.state = {}
-
+    this.state = {
+      contentAry: []
+    }
   }
 
   componentDidMount () {
@@ -33,21 +37,33 @@ export default class SelectUserView extends Component<{}> {
   }
 
   _bootstrapAsync = async () => {
+    const {navigation} = this.props
     const currentUser = lkApplication.getCurrentUser()
     const userAry = await userProvider.asyGetAll()
-    const {length} = userAry
     console.log({userAry, currentUser})
-    if (length !== 0) {
-
-    }
+    const data = userAry.reduce((accumulator, ele) => {
+      const {pic, id, name, serverIP, serverPort} = ele
+      const obj = {
+        image: getAvatarSource(pic),
+        id,
+        name: `${name}`,
+        content: `IP地址: ${serverIP} 端口: ${serverPort} 标识: ${id}`,
+        onPress: () => {
+          navigation.navigate('PasswordLoginView', {user: ele})
+        }
+      }
+      accumulator.push(obj)
+      return accumulator
+    }, [])
+    this.setState({
+      contentAry: <MessageList data={data}></MessageList>
+    })
   }
 
   render () {
     return (
-      <View>
-        <Text>
-          selectUser
-        </Text>
+      <View style={{marginVertical: 20}}>
+        {this.state.contentAry}
       </View>
     )
   }
