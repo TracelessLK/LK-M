135277c74733644aac352268b7997cdc867a2d08
spirit <catchspirit37@gmail.com>
2018-10-19 13:10:25 +0800

    z - Fri Oct 19 2018 13:10:24 GMT+0800 (CST)

--- android/app/src/main/java/com/lk_m/MainApplication.java
+++ android/app/src/main/java/com/lk_m/MainApplication.java
@@ -19,7 +19,7 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import cn.reactnative.modules.update.UpdateContext;
 import org.pgsqlite.SQLitePluginPackage;
-
+import com.lk_m.nativeJava.AnExampleReactPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -50,7 +50,8 @@ public class MainApplication extends Application implements ReactApplication {
             new RNDeviceInfo(),
             new VectorIconsPackage(),
             new RNFSPackage(),
-            new SQLitePluginPackage()
+            new SQLitePluginPackage(),
+            new AnExampleReactPackage()
       );
     }
 
