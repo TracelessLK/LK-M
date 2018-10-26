e2de980aae15587fc0c5290a62e74d2ff684335a
spirit <catchspirit37@gmail.com>
2018-10-26 15:12:12 +0800

    z - Fri Oct 26 2018 15:12:11 GMT+0800 (CST)

--- config/devConfig.js
+++ config/devConfig.js
@@ -1,7 +1,7 @@
 
 const devConfig = {
   appId: 'LK_M',
-  appName: 'LK2',
+  appName: 'LK',
   exportIpaFolderPath: '/Users/spirit/entry/git/working_on/LK-S/static/public/ios',
   exportPPKFolderPath: '/Users/spirit/entry/git/working_on/LK-S/static/public/ppk',
   exportApkFolderPath: '/Users/spirit/entry/git/working_on/LK-S/static/public/android'
