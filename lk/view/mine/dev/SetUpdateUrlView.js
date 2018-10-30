76dc893d3fd18ba6d01be06b65b79f0c20a2978e
spirit <catchspirit37@gmail.com>
2018-10-30 15:27:35 +0800

    z - Tue Oct 30 2018 15:27:35 GMT+0800 (CST)

--- lk/view/mine/dev/SetUpdateUrlView.js
+++ lk/view/mine/dev/SetUpdateUrlView.js
@@ -2,10 +2,11 @@ import React, {Component} from 'react'
 import {
   Text,
   View,
-  AsyncStorage
+  AsyncStorage,
+  TextInput
 } from 'react-native'
 import {
-  Card
+  Card, CardItem, Button
 } from 'native-base'
 import PropTypes from 'prop-types'
 
@@ -17,25 +18,55 @@ export default class SetUpdateUrlView extends Component<{}> {
 
   async componentDidMount () {
     const updateUrlBase = await AsyncStorage.getItem('updateUrlBase')
-    console.log({updateUrlBase})
+    // console.log({updateUrlBase})
+    this.setState({
+      updateUrlBase
+    })
+  }
+  save = async () => {
+    // console.log(this.t)
+    await AsyncStorage.setItem('updateUrlBase', this.t)
+    const updateUrlBase = await AsyncStorage.getItem('updateUrlBase')
     this.setState({
       updateUrlBase
     })
   }
 
-  componentWillUnmount () {
-
-  }
-
   render () {
     return (
-      <View>
-        <Card style={{justifyContent: 'center', alignItems: 'center', padding: 20, margin: 20}}>
+      <View style={{justifyContent: 'center', alignItems: 'center'}}>
+        <Card style={{justifyContent: 'center', alignItems: 'center', padding: 20, margin: 20, width: '90%'}}>
           <Text>{this.state.updateUrlBase || '没有设置'}</Text>
         </Card>
-        <View>
+        <Card style={{width: '90%', marginTop: 40}}>
+          <CardItem header bordered>
+            <Text>设置自定义更新网址</Text>
+          </CardItem>
+          <CardItem bordered>
+            <View style={{justifyContent: 'center', flexDirection: 'row', alignItems: 'center'}}>
+              <TextInput style={{
+                flex: 1,
+                color: 'black',
+                fontSize: 16,
+                paddingHorizontal: 4,
+                borderWidth: 1,
+                borderColor: '#d0d0d0',
+                borderRadius: 5,
+                marginHorizontal: 5,
+                minHeight: 40,
+                backgroundColor: '#f0f0f0',
+                marginBottom: 5
+              }} autoFocus onChangeText={t => {
+                this.t = t.trim()
+              }} onSubmitEditing={this.save}
+              ></TextInput>
+              <Button block info style={{width: 60, height: 40}} onPress={this.save}>
+                <Text style={{color: 'white'}}>确认</Text>
+              </Button>
+            </View>
 
-        </View>
+          </CardItem>
+        </Card>
       </View>
     )
   }
