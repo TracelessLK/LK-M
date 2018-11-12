19f7e186cac93083f7e5c75d1f19f9f8767da1d5
spirit <catchspirit37@gmail.com>
2018-11-12 16:26:42 +0800

    z - Mon Nov 12 2018 16:26:41 GMT+0800 (CST)

--- lk/view/external/NotifyView.js
+++ lk/view/external/NotifyView.js
@@ -1,31 +1,49 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import {
   Text,
-  View
-} from 'react-native';
+  View,
+  StatusBar, Image
+} from 'react-native'
+const style = require('../style')
+const backImg = require('../image/back-icon.png')
 
 export default class NotifyView extends Component<{}> {
-
   static navigationOptions = () => {
     return {
-      headerTitle: "通知"
+      headerTitle: '通知',
+      headerBackground: (
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor={style.color.mainColor}
+        />
+      ),
+      headerStyle: {
+        backgroundColor: '#303030'
+      },
+      headerTitleStyle: {
+        color: 'white'
+      },
+      headerBackTitleStyle: {
+        color: 'white'
+      },
+      headerBackImage: (
+        <Image style={{width: 30, height: 30}} source={backImg}></Image>
+      )
     }
   }
 
-  constructor(props) {
-    super(props);
-    this.state = {};
+  constructor (props) {
+    super(props)
+    this.state = {}
   }
 
-
-  render() {
+  render () {
     return (
       <View>
         <Text>ExternalView</Text>
       </View>
     )
   }
-
 }
 
 NotifyView.defaultProps = {}
