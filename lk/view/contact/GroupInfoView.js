d01f66a50a5fdf1c96d2afa64d714a2be6604323
admin <admin@leezizideMacBook-Air.local>
2019-04-10 10:07:43 +0800

    成员信息中显示增加人员

--- lk/view/contact/GroupInfoView.js
+++ lk/view/contact/GroupInfoView.js
@@ -25,41 +25,62 @@ export default class GroupInfoView extends Component<{}> {
     return {
       headerTitle: '成员信息',
       headerRight:
-        <TouchableOpacity onPress={navigation.getParam('addMember')}>
-          <Image source={addPng} style={{width: size, height: size, marginHorizontal: 10}} resizeMode='contain'/>
-        </TouchableOpacity>
+          <TouchableOpacity onPress={navigation.getParam('addMember')}>
+            <Image source={addPng} style={{width: size, height: size, marginHorizontal: 10}} resizeMode='contain'/>
+          </TouchableOpacity>
     }
   }
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      groups:[]
+    }
     this.group = this.props.navigation.state.params.group
     // console.log({group: this.group})
     this.user = lkApp.getCurrentUser()
+    this.asyncRender(this.group.id)
   }
 
   componentDidMount () {
     const {navigation} = this.props
     navigation.setParams({addMember: () => {
-      navigation.navigate('AddGroupMemberView', {group: this.group})
-    }})
+        navigation.navigate('AddGroupMemberView', {group: this.state.groups})
+      }})
     chatManager.on('groupMemberChange', this.groupMemberChange)
   }
 
-  groupMemberChange = (chatId) => {
+  groupMemberChange = async (chatId) => {
     if (chatId === this.group.id) {
       this.setState({
         update: true
       })
     }
+    this.asyncRender(chatId)
   }
-
+  async asyncRender (filterText) {
+    const chat = await chatManager.asyGetChat(this.user.id, filterText)
+    let headerTitle = chat.name
+    const memberAry = await chatManager.asyGetGroupMembers(filterText)
+    let memberInfoObj = memberAry.reduce((accumulator, ele) => {
+      accumulator[ele.id] = ele
+      return accumulator
+    }, {})
+    let o = {
+      memberInfoObj,
+      id: filterText,
+      name: headerTitle
+    }
+    this.setState({
+      groups: o
+    })
+  }
   componentWillUnmount () {
     chatManager.un('groupMemberChange', this.groupMemberChange)
   }
 
   render () {
-    const {memberInfoObj} = this.group
+    const state = this.state
+    const {memberInfoObj} = state.groups
     const dataAry = []
     for (let key in memberInfoObj) {
       const value = memberInfoObj[key]
@@ -77,28 +98,28 @@ export default class GroupInfoView extends Component<{}> {
       dataAry.push(obj)
     }
     return (
-      <ScrollView>
-        {this.state.isWating ? <ActivityIndicator size='large' style={{position: 'absolute', top: '50%'}}/> : null}
-        <View style={{marginVertical: 20}}>
-          <List data={dataAry}></List>
-          <TouchableOpacity style={{backgroundColor: 'white',
-            marginVertical: 60,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            padding: 12}} onPress={() => {
-            runNetFunc(async () => {
-              this.setState({
-                isWating: true
+        <ScrollView>
+          {this.state.isWating ? <ActivityIndicator size='large' style={{position: 'absolute', top: '50%'}}/> : null}
+          <View style={{marginVertical: 20}}>
+            <List data={dataAry}></List>
+            <TouchableOpacity style={{backgroundColor: 'white',
+              marginVertical: 60,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              padding: 12}} onPress={() => {
+              runNetFunc(async () => {
+                this.setState({
+                  isWating: true
+                })
+                await chatManager.leaveGroup(this.group.id)
+                this.props.navigation.navigate('RecentTab')
               })
-              await chatManager.leaveGroup(this.group.id)
-              this.props.navigation.navigate('RecentTab')
-            })
-          }}>
-            <Text style={{fontSize: 17, color: '#e53d59'}} >退出群聊</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
+            }}>
+              <Text style={{fontSize: 17, color: '#e53d59'}} >退出群聊</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
 
     )
   }
