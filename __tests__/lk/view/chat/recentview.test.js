81efbe5baae287fab0573634cbf1ba58d592fb6b
admin <admin@leezizideMacBook-Air.local>
2019-07-09 15:41:14 +0800

    fix test

--- __tests__/lk/view/chat/recentview.test.js
+++ __tests__/lk/view/chat/recentview.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import 'react-native'
 import renderer from 'react-test-renderer'
 import RecentView from '../../../../lk/view/chat/RecentView'
+import "react-native"
 
 jest.mock('react-native-fetch-blob', () => 'RNFetchBlob')
 jest.mock('react-native-device-info', () => 'DeviceInfo')
@@ -16,8 +16,11 @@ jest.mock('react-native-update', () => {
     markSuccess: jest.fn()
   }
 })
-
-describe('RecentView', () => {
+jest.setTimeout(1000)
+describe('RecentView tests', () => {
+  beforeEach(() => {
+    jest.resetModules()
+  })
   let props = {
     navigation: {
       navigate: jest.fn(),
@@ -30,23 +33,83 @@ describe('RecentView', () => {
     )
     expect(tree.toJSON()).toMatchSnapshot()
   })
-  // describe('call function', () => {
-  //   it('should render a asyGetAllDetainedMsg ready', () => {
-  //     const wrapper = shallow(<RecentView {...props}/>)
-  //     let option = {refreshControl: true, showWarning: true, minTime: 1000 }
-  //     expect(wrapper.asyGetAllDetainedMsg(option)).toMatchSnapshot()
-  //   })
-  //   it('defaultContent function', () => {
-  //     const wrapper = shallow(<RecentView {...props}/>)
-  //     expect(wrapper.getDefaultContent()).toMatchSnapshot()
-  //   })
-  //   it('getConnectionMsg function', () => {
-  //     const wrapper = shallow(<RecentView {...props}/>)
-  //     expect(wrapper.getConnectionMsg()).toMatchSnapshot()
-  //   })
-  //   it('connectionOpen function', () => {
-  //     const wrapper = shallow(<RecentView {...props}/>)
-  //     expect(wrapper.connectionOpen()).toMatchSnapshot()
-  //   })
-  // })
+  describe('call function', () => {
+    it('onBackPress', () => {
+      const node = new RecentView(props)
+      expect(node.onBackPress()).toMatchSnapshot()
+    })
+    it('optionToChoose', () => {
+      const node = new RecentView(props)
+      expect(node.optionToChoose()).toMatchSnapshot()
+    })
+    it('updateListener', () => {
+      const node = new RecentView(props)
+      expect(node.updateListener()).toMatchSnapshot()
+    })
+    it('msgBadgeChangedListener', () => {
+      const node = new RecentView(props)
+      let params = {
+        param: jest.fn()
+      }
+      expect(node.msgBadgeChangedListener(params)).toMatchSnapshot()
+    })
+    it('netStateChangedListener', () => {
+      const node = new RecentView(props)
+      let option = {
+        param: jest.fn()
+      }
+      expect(node.netStateChangedListener(option)).toMatchSnapshot()
+    })
+    it('componentDidMount', () => {
+      const node = new RecentView(props)
+      expect(node.componentDidMount()).toMatchSnapshot()
+    })
+    it('_handleAppStateChange', () => {
+      const node = new RecentView(props)
+      let appState = jest.fn()
+      expect(node._handleAppStateChange(appState)).toMatchSnapshot()
+    })
+    it('connectionFail', () => {
+      const node = new RecentView(props)
+      expect(node.connectionFail()).toMatchSnapshot()
+    })
+    it('getDefaultContent', () => {
+      const node = new RecentView(props)
+      expect(node.getDefaultContent()).toMatchSnapshot()
+    })
+    it('updateRecent', () => {
+      const node = new RecentView(props)
+      node.setState({
+        contentAry: jest.fn(),
+        refreshing: jest.fn()
+      })
+      expect(node.updateRecent()).toMatchSnapshot()
+    })
+    it('addActivityIndicator', () => {
+      const node = new RecentView(props)
+      expect(node.addActivityIndicator()).toMatchSnapshot()
+    })
+    it('asyGetAllDetainedMsg', () => {
+      const node = new RecentView(props)
+      const option = jest.fn()
+      expect(node.asyGetAllDetainedMsg(option)).toMatchSnapshot()
+    })
+    it('resetHeaderTitle', () => {
+      const node = new RecentView(props)
+      const num = jest.fn()
+      expect(node.resetHeaderTitle(num)).toMatchSnapshot()
+    })
+    it('getConnectionMsg', () => {
+      const node = new RecentView(props)
+      expect(node.getConnectionMsg()).toBe('当前网络不可用,请检查您的网络设置')
+    })
+    it('componentWillUnmount', () => {
+      const node = new RecentView(props)
+      node.setState({
+        contentAry: jest.fn(),
+        refreshing: jest.fn()
+      })
+      expect(node.componentWillUnmount()).toMatchSnapshot()
+    })
+  })
 })
